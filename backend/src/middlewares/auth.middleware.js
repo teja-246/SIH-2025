ebc85b9b127af7f6226c0b10e602c010762fb757
T. Teja <tteja375@gmail.com>
2025-10-06 20:34:05 +0530

    setting up backend

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,37 @@
+import { ApiError } from "../utils/apiError.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import jwt from "jsonwebtoken"
+import { User } from "../models/user.model.js"
+
+export const verifyJWT = asyncHandler(async(req,res,next)=>{
+    try {
+        console.log("Verifying JWT..."); // Debug log
+               
+        console.log("Cookies received:", req.cookies); // Debug cookies
+        console.log("Headers:", req.headers); // Debug headers
+        
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+    
+        if(!token){
+            throw new ApiError(401, "Unauthorized Request in VerifyJWT")
+        }
+
+        console.log("Token found:", token); // Debug token
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+        console.log("Decoded token:", decodedToken); // Debug decoded token
+        
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+    
+        if(!user){
+            throw new ApiError(401, "Invalid Access Token")
+        }
+    
+        req.user = user;
+        next()
+    } 
+    catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token")
+    }
+})
